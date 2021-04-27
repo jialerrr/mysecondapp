8c0c7ff89df1f2622370c4f4293cd3bcc3d2fa1d
jialer <20021576@myrp.edu.sg>
2021-04-27 13:41:20 +0800

    Initial commit

---
+++ app/src/main/java/rp/edu/sg/c346/id20021576/demomysecondapp/MainActivity.java
@@ -0,0 +1,14 @@
+package rp.edu.sg.c346.id20021576.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
